b6a04a046c710485d83f2d1ece9b1681fb5f7fc4
anand-sri <anand.dhuli423@gmail.com>
2021-02-26 16:18:06 +0530

    lkh

--- print.sh
+++ print.sh
@@ -3,3 +3,11 @@
 echo hello
 
 echo -e "hi all\n\good morning welcome the meeeting\n\regards\n\anand"
+
+
+echo -e "\e[31mRED TEXT"
+echo -e "\e[32mGREEN TEXT"
+echo -e "\e[33mYELLOW TEXT"
+echo -e "\e[34mBLUE TEXT"
+echo -e "\e[35mMAGENTA TEXT"
+echo -e "\e[36mCYNA TEXT "
