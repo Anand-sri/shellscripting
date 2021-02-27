126792ff141beccdec05754b5caf685727cb7c04
anand-sri <anand.dhuli423@gmail.com>
2021-02-27 11:19:39 +0530

    lkh

---
+++ variables.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+
+Hi welocme to anand training instute
+
+learning course anand training
+
+anand course learnng
+
+COURSE_NAME=anand
+
+
+Hi welocme to COURSE_NAME training instute
+
+learning COURSE_NAME anand training
+
+anand COURSE_NAME learnng
+
