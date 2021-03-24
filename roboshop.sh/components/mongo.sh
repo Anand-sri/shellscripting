2e7c2d7545961f7b2b59c86b5ea80fc17e657b25
anand-sri <anand.dhuli423@gmail.com>
2021-03-24 11:57:52 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop.sh/components/mongo.sh
+++ roboshop.sh/components/mongo.sh
@@ -25,10 +25,11 @@ systemctl enable mongod  &>>$LOG_FILE
 systemctl start mongod   &>>$LOG_FILE
 
 INFO " Downloading the mongodb shema"
-DOWNLOAD_ARTIFACT"https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+
 cd /tmp
 INFO "Extract the mongodb schema"
-unzip -o mongodb.zip &>>$LOG_FILE
+unzip -o mongo.zip &>>$LOG_FILE
 STAT $? "extract the mongodb schema"
 INFO "Load Schema - Catalogue Service"
 mongo < catalogue.js &>>$LOG_FILE
