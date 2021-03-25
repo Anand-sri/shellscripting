d108cdf00226fb14bd770732ecae831969ab09be
anand-sri <anand.dhuli423@gmail.com>
2021-03-25 17:13:35 +0530

    KJ

--- roboshop.sh/components/User.sh
+++ roboshop.sh/components/User.sh
@@ -13,10 +13,11 @@ su roboshop
 DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/8cd1d535-7b52-4823-9003-7b52db898c08/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 cd /home/roboshop
 mkdir user
-
-INFO "unzip the user.zip files"
-unzip /tmp/user.zip &>>$LOG_FILE
-STAT $? "unzip the user files"
+INFO "Extract Artifacts"
+mkdir -p /home/roboshop/${COMPONENT}
+cd /home/roboshop/${COMPONENT}
+unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
+STAT $? "Artifacts Extract"
 chown roboshop:roboshop /home/roboshop/${COMPONENT} -R
 INFO "Install NodeJS dependencies"
 npm install --unsafe-perm  &>>$LOG_FILE
